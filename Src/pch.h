a25ae63a6a5943efe7847511755b69c856a52965
Chuck Walbourn <chuckw@windows.microsoft.com>
2017-07-14 16:45:57 -0700

    Build library with Wall

--- Src/pch.h
+++ Src/pch.h
@@ -13,6 +13,34 @@
 
 #pragma once
 
+// Off by default warnings
+#pragma warning(disable : 4061 4265 4355 4365 4571 4623 4625 4626 4668 4710 4711 4746 4774 4820 4987 5026 5027 5031 5032)
+// C4061 enumerator 'X' in switch of enum 'X' is not explicitly handled by a case label
+// C4265 class has virtual functions, but destructor is not virtual
+// C4355 'this': used in base member initializer list
+// C4365 signed/unsigned mismatch
+// C4571 behavior change
+// C4623 default constructor was implicitly defined as deleted
+// C4625 copy constructor was implicitly defined as deleted
+// C4626 assignment operator was implicitly defined as deleted
+// C4668 not defined as a preprocessor macro
+// C4710 function not inlined
+// C4711 selected for automatic inline expansion
+// C4746 volatile access of '<expression>' is subject to /volatile:<iso|ms> setting
+// C4774 format string expected in argument 3 is not a string literal
+// C4820 padding added after data member
+// C4987 nonstandard extension used
+// C5026 move constructor was implicitly defined as deleted
+// C5027 move assignment operator was implicitly defined as deleted
+// C5031/5032 push/pop mismatches in windows headers
+
+// XBox One XDK related Off by default warnings
+#pragma warning(disable : 4619 4471 4917 4986)
+// C4619 #pragma warning: there is no warning number 'X'
+// C4471 forward declaration of an unscoped enumeration must have an underlying type
+// C4917 a GUID can only be associated with a class, interface or namespace
+// C4986 exception specification does not match previous declaration
+
 #pragma warning(push)
 #pragma warning(disable : 4005)
 #define WIN32_LEAN_AND_MEAN
