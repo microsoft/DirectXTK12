f1ff90ada5ad4b65a442c039b54dc8702b496ce9
Chuck Walbourn <chuckw@windows.microsoft.com>
2018-04-24 00:32:58 -0700

    Turned off more VS 2017 15.7 warnings

--- Src/pch.h
+++ Src/pch.h
@@ -10,7 +10,7 @@
 #pragma once
 
 // Off by default warnings
-#pragma warning(disable : 4619 4061 4265 4355 4365 4571 4623 4625 4626 4628 4668 4710 4711 4746 4774 4820 4987 5026 5027 5031 5032 5039)
+#pragma warning(disable : 4619 4061 4265 4355 4365 4571 4623 4625 4626 4628 4668 4710 4711 4746 4774 4820 4987 5026 5027 5031 5032 5039 5045)
 // C4619 #pragma warning: there is no warning number 'X'
 // C4061 enumerator 'X' in switch of enum 'X' is not explicitly handled by a case label
 // C4265 class has virtual functions, but destructor is not virtual
@@ -32,6 +32,7 @@
 // C5027 move assignment operator was implicitly defined as deleted
 // C5031/5032 push/pop mismatches in windows headers
 // C5039 pointer or reference to potentially throwing function passed to extern C function under - EHc
+// C5045 Spectre mitigation warning
 
 // XBox One XDK related Off by default warnings
 #pragma warning(disable : 4471 4917 4986 5043)
