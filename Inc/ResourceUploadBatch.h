5be1f2eabb0479c2cd079200884b94a0eb1174db
Chuck Walbourn <chuckw@windows.microsoft.com>
2020-04-10 13:15:49 -0700

    ResourceUploadBatch updated to support copy & compute queues

--- Inc/ResourceUploadBatch.h
+++ Inc/ResourceUploadBatch.h
@@ -38,7 +38,7 @@ namespace DirectX
         virtual ~ResourceUploadBatch();
 
         // Call this before your multiple calls to Upload.
-        void __cdecl Begin();
+        void __cdecl Begin(D3D12_COMMAND_LIST_TYPE commandType = D3D12_COMMAND_LIST_TYPE_DIRECT);
 
         // Asynchronously uploads a resource. The memory in subRes is copied.
         // The resource must be in the COPY_DEST state.
@@ -66,8 +66,7 @@ namespace DirectX
         // Submits all the uploads to the driver.
         // No more uploads can happen after this call until Begin is called again.
         // This returns a handle to an event that can be waited on.
-        std::future<void> __cdecl End(
-            _In_ ID3D12CommandQueue* commandQueue);
+        std::future<void> __cdecl End(_In_ ID3D12CommandQueue* commandQueue);
 
         // Validates if the given DXGI format is supported for autogen mipmaps
         bool __cdecl IsSupportedForGenerateMips(DXGI_FORMAT format) noexcept;
