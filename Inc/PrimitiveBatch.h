58f5c613ef922db77a9f1a1a90bb32290c897aa9
Chuck Walbourn <chuckw@microsoft.com>
2020-05-20 19:53:46 -0700

    Fixed C4625/C4626 warning

--- Inc/PrimitiveBatch.h
+++ Inc/PrimitiveBatch.h
@@ -77,6 +77,8 @@ namespace DirectX
             return *this;
         }
 
+        PrimitiveBatch(PrimitiveBatch const&) = delete;
+        PrimitiveBatch& operator= (PrimitiveBatch const&) = delete;
 
         // Similar to the D3D9 API DrawPrimitiveUP.
         void Draw(D3D_PRIMITIVE_TOPOLOGY topology, _In_reads_(vertexCount) TVertex const* vertices, size_t vertexCount)
