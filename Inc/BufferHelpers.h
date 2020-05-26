42f5a20ec7b594ec5afd41f1de0eb1284a5d9a1f
Chuck Walbourn <chuckw@windows.microsoft.com>
2020-05-26 02:03:21 -0700

    BufferHelpers added to the toolkit

---
+++ Inc/BufferHelpers.h
@@ -0,0 +1,78 @@
+//--------------------------------------------------------------------------------------
+// File: BufferHelpers.h
+//
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+//
+// http://go.microsoft.com/fwlink/?LinkID=615561
+//--------------------------------------------------------------------------------------
+
+#pragma once
+
+#if defined(_XBOX_ONE) && defined(_TITLE)
+#include <d3d12_x.h>
+#else
+#include <d3d12.h>
+#endif
+
+
+namespace DirectX
+{
+    class ResourceUploadBatch;
+
+    // Helpers for creating initialized Direct3D buffer resources.
+    HRESULT __cdecl CreateStaticBuffer(_In_ ID3D12Device* device,
+        ResourceUploadBatch& resourceUpload,
+        _In_reads_bytes_(count* stride) const void* ptr,
+        size_t count,
+        size_t stride,
+        D3D12_RESOURCE_STATES afterState,
+        _COM_Outptr_ ID3D12Resource** pBuffer) noexcept;
+
+    template<typename T>
+    HRESULT CreateStaticBuffer(_In_ ID3D12Device* device,
+        ResourceUploadBatch& resourceUpload,
+        _In_reads_(count) T const* data,
+        size_t count,
+        D3D12_RESOURCE_STATES afterState,
+        _COM_Outptr_ ID3D12Resource** pBuffer) noexcept
+    {
+        return CreateStaticBuffer(device, resourceUpload, data, count, sizeof(T), afterState, pBuffer);
+    }
+
+    template<typename T>
+    HRESULT CreateStaticBuffer(_In_ ID3D12Device* device,
+        ResourceUploadBatch& resourceUpload,
+        T const& data,
+        D3D12_RESOURCE_STATES afterState,
+        _COM_Outptr_ ID3D12Resource** pBuffer) noexcept
+    {
+        return CreateStaticBuffer(device, resourceUpload, data.data(), data.size(), sizeof(typename T::value_type), afterState, pBuffer);
+    }
+
+    // Helpers for creating texture from memory arrays.
+    HRESULT __cdecl CreateTextureFromMemory(_In_ ID3D12Device* device,
+        ResourceUploadBatch& resourceUpload,
+        size_t width,
+        DXGI_FORMAT format,
+        const D3D12_SUBRESOURCE_DATA& initData,
+        _COM_Outptr_ ID3D12Resource** texture,
+        D3D12_RESOURCE_STATES afterState = D3D12_RESOURCE_STATE_PIXEL_SHADER_RESOURCE);
+
+    HRESULT __cdecl CreateTextureFromMemory(_In_ ID3D12Device* device,
+        ResourceUploadBatch& resourceUpload,
+        size_t width, size_t height,
+        DXGI_FORMAT format,
+        const D3D12_SUBRESOURCE_DATA& initData,
+        _COM_Outptr_ ID3D12Resource** texture,
+        bool generateMips = false,
+        D3D12_RESOURCE_STATES afterState = D3D12_RESOURCE_STATE_PIXEL_SHADER_RESOURCE);
+
+    HRESULT __cdecl CreateTextureFromMemory(_In_ ID3D12Device* device,
+        ResourceUploadBatch& resourceUpload,
+        size_t width, size_t height, size_t depth,
+        DXGI_FORMAT format,
+        const D3D12_SUBRESOURCE_DATA& initData,
+        _COM_Outptr_ ID3D12Resource** texture,
+        D3D12_RESOURCE_STATES afterState = D3D12_RESOURCE_STATE_PIXEL_SHADER_RESOURCE);
+}
