c644182d63c12ccb4db23b2ad90c6ce048efb7ef
Chuck Walbourn <chuckw@microsoft.com>
2021-09-30 01:08:48 -0700

    Fixed URL

--- Src/ModelLoadCMO.cpp
+++ Src/ModelLoadCMO.cpp
@@ -4,7 +4,7 @@
 // Copyright (c) Microsoft Corporation.
 // Licensed under the MIT License.
 //
-// http://go.microsoft.com/fwlink/?LinkId=248929
+// http://go.microsoft.com/fwlink/?LinkID=615561
 //--------------------------------------------------------------------------------------
 
 #include "pch.h"
