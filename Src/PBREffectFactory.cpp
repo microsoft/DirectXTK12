e107f860e6eb801cbee67bf4cbdf0cab0d87a74c
Chuck Walbourn <chuckw@microsoft.com>
2021-10-13 13:56:42 -0700

    Minor SAL updates

--- Src/PBREffectFactory.cpp
+++ Src/PBREffectFactory.cpp
@@ -24,7 +24,7 @@ namespace
 {
     template<typename T>
     void SetPBRProperties(
-        T* effect,
+        _In_ T* effect,
         const EffectFactory::EffectInfo& info,
         _In_ DescriptorHeap* textures,
         int textureDescriptorOffset,
