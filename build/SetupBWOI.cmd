f5bf946526e57b20ffac79ed25326580e204c992
Chuck Walbourn <chuckw@microsoft.com>
2024-05-03 19:18:49 -0700

    YAML code cleanup

--- build/SetupBWOI.cmd
+++ build/SetupBWOI.cmd
@@ -37,10 +37,6 @@ echo ADDLIB: %ADDLIB%
 set PATH=%ADDBIN%;%PATH%
 set INCLUDE=%INCLUDE%;%ADDINCLUDE%
 set LIB=%LIB%;%ADDLIB%
-
-echo ##vso[task.setvariable variable=INCLUDE;]%INCLUDE%
-echo ##vso[task.setvariable variable=LIB;]%LIB%
-echo ##vso[task.prependpath]%ADDBIN%
 exit /b 0
 
 :needpackage
